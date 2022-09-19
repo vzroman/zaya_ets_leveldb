636fe6ba63093cdec9953d93443e73f7f9f6c732
roman <vzroman@gmail.com>
2022-09-19 07:59:55 +0600

    clean up

--- src/zaya_ets_leveldb.erl
+++ src/zaya_ets_leveldb.erl
@@ -1,30 +1,6 @@
 
 -module(zaya_ets_leveldb).
 
--ifndef(TEST).
-
--define(LOGERROR(Text),lager:error(Text)).
--define(LOGERROR(Text,Params),lager:error(Text,Params)).
--define(LOGWARNING(Text),lager:warning(Text)).
--define(LOGWARNING(Text,Params),lager:warning(Text,Params)).
--define(LOGINFO(Text),lager:info(Text)).
--define(LOGINFO(Text,Params),lager:info(Text,Params)).
--define(LOGDEBUG(Text),lager:debug(Text)).
--define(LOGDEBUG(Text,Params),lager:debug(Text,Params)).
-
--else.
-
--define(LOGERROR(Text),ct:pal("error: "++Text)).
--define(LOGERROR(Text,Params),ct:pal("error: "++Text,Params)).
--define(LOGWARNING(Text),ct:pal("warning: "++Text)).
--define(LOGWARNING(Text,Params),ct:pal("warning: "++Text,Params)).
--define(LOGINFO(Text),ct:pal("info: "++Text)).
--define(LOGINFO(Text,Params),ct:pal("info: "++Text,Params)).
--define(LOGDEBUG(Text),ct:pal("debug: "++Text)).
--define(LOGDEBUG(Text,Params),ct:pal("debug: "++Text,Params)).
-
--endif.
-
 %%=================================================================
 %%	SERVICE API
 %%=================================================================
